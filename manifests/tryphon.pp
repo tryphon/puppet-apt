d2a4363e664f2d1e9df31650f003f16455bfd53f
Alban Peignier <alban.peignier@free.fr>
2010-09-06 19:00:14 +0200

    Use an optionnal apt_tryphon_host variable to override tryphon debian repository hostname

--- manifests/tryphon.pp
+++ manifests/tryphon.pp
@@ -1,11 +1,14 @@
 class apt::tryphon {
+  $real_apt_tryphon_host = $apt_tryphon_host ? {
+    default => $apt_tryphon_host,
+    '' => "debian.tryphon.eu"
+  } 
+
   apt::sources_list { tryphon:
-    content => "deb http://debian.tryphon.eu lenny main contrib
-deb http://debian.tryphon.eu lenny-backports main contrib
-"
+    content => "deb http://$real_apt_tryphon_host lenny main contrib\ndeb http://$real_apt_tryphon_host lenny-backports main contrib\n"
   }
 
   apt::key { "C6ADBBD5":
-    source => "http://debian.tryphon.eu/release.asc"
+    source => "http://$real_apt_tryphon_host/release.asc"
   }
 }
