e2922c1bcb08b9a28e8d20c1c767c24b531baa8f
Alban Peignier <alban.peignier@free.fr>
2010-09-06 11:59:25 +0200

    Update tryphon repository url (debian.tryphon.eu now)

--- manifests/tryphon.pp
+++ manifests/tryphon.pp
@@ -1,11 +1,11 @@
 class apt::tryphon {
   apt::sources_list { tryphon:
-    content => "deb http://debian.tryphon.org lenny main contrib
-deb http://debian.tryphon.org lenny-backports main contrib
+    content => "deb http://debian.tryphon.eu lenny main contrib
+deb http://debian.tryphon.eu lenny-backports main contrib
 "
   }
 
   apt::key { "C6ADBBD5":
-    source => "http://debian.tryphon.org/release.asc"
+    source => "http://debian.tryphon.eu/release.asc"
   }
 }
