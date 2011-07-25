46519826403357661902afe3a288b9fc467bbe2d
Alban Peignier <alban@tryphon.eu>
2011-07-25 09:24:07 +0200

    Include wget before retrieving key

--- manifests/definitions/key.pp
+++ manifests/definitions/key.pp
@@ -2,11 +2,13 @@ define apt::key($ensure = present, $source) {
   case $ensure {
     present: {
       if $source =~ /^http:/ {
+        include wget
         exec { "/usr/bin/wget -O - '$source' | /usr/bin/apt-key add -":
           unless => "apt-key list | grep -Fqe '${name}'",
           path   => "/bin:/usr/bin",
           before => Exec["apt-get_update"],
           notify => Exec["apt-get_update"],
+          require => Package[wget]
         }
       } else {
         file { "/etc/apt/keys": 
